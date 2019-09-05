21c06428101af88f03797f81aeb97f4162d858cc
Alonso Ahmetaj <onsomac@Alonsos-MacBook-Pro.local>
2019-09-04 22:54:10 -0400

    add homepage comp and styles scss, use the comps within app comp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import HomePage from "./homepage.component";
+
 function App() {
-  return <div>Clothes Ecom Project</div>;
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+  );
 }
 
 export default App;
