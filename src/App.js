a9870b1c0f84418ad8f347f4812f8d6842ab136b
Alonso Ahmetaj <onsomac@Alonsos-MacBook-Pro.local>
2019-09-21 01:54:37 -0400

    testing for github

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
+//checking git auth test
+//writing some tests for the github account
 
 function App() {
   return (
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,6 +4,7 @@ import Directory from "../../components/directory/directory.component";
 import "./homepage.styles.scss";
 
 const HomePage = () => (
+  //some commments for testing github
   <div className="homepage">
     <Directory></Directory>
   </div>
