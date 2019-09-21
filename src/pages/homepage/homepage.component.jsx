ebf494a804d18eb00d94b60ba837d93b0e12abd8
Alonso Ahmetaj <onsomac@Alonsos-MacBook-Pro.local>
2019-09-21 02:44:46 -0400

    testing again test2

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,6 +4,7 @@ import Directory from "../../components/directory/directory.component";
 import "./homepage.styles.scss";
 
 const HomePage = () => (
+  //testing again github ssh connection
   <div className="homepage">
     <Directory></Directory>
   </div>
