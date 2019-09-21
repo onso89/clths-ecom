c9e471c6ebb197babb77ad459f219076a82d3702
Alonso Ahmetaj <onsomac@Alonsos-MacBook-Pro.local>
2019-09-21 02:36:16 -0400

    delete some comments

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,7 +4,6 @@ import Directory from "../../components/directory/directory.component";
 import "./homepage.styles.scss";
 
 const HomePage = () => (
-  //some commments for testing github
   <div className="homepage">
     <Directory></Directory>
   </div>
