85e61599d461eac76df7f5409d8c6bb30248cb48
Alonso Ahmetaj <alonsoahmetaj@gmail.com>
2019-09-21 22:38:47 -0400

    removing some comments

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,7 +4,6 @@ import Directory from "../../components/directory/directory.component";
 import "./homepage.styles.scss";
 
 const HomePage = () => (
-  //testing email change on git local
   <div className="homepage">
     <Directory></Directory>
   </div>
