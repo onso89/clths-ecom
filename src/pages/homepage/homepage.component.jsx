076b7b365f769d04af4012d9c45d12265576624f
Alonso Ahmetaj <alonsoahmetaj@gmail.com>
2019-09-21 22:36:42 -0400

    check git email update

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,6 +4,7 @@ import Directory from "../../components/directory/directory.component";
 import "./homepage.styles.scss";
 
 const HomePage = () => (
+  //testing email change on git local
   <div className="homepage">
     <Directory></Directory>
   </div>
